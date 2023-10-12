6a4cb6fee048cbeb3b905779edb70aca81ea7738
Iván Castañeda <icastaneda@scba.gov.ar>
2023-10-12 00:16:43 -0300

    Incorporacion de metodos, algunos son utilizados en documentacion (https://github.com/Pericial/GAP/issues/57).

--- SqlOrganize/DAO.cs
+++ SqlOrganize/DAO.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utils;
 
 namespace SqlOrganize
 {
@@ -31,6 +32,50 @@ namespace SqlOrganize
         public IDictionary<string, object> Get(string entityName, object id)
         {
             return Db.Query(entityName).CacheByIds(id).ElementAt(0);
+        }
+
+        public IDictionary<string, object>? RowByFieldValue(string entityName, string fieldName, object value)
+        {
+            return Db.Query(entityName).Where("$" + fieldName + " = @0").Parameters(value).DictCache();
+        }
+
+        public IDictionary<string, object>? RowByUniqueWithoutIdIfExists(string entityName, IDictionary<string, object?> source)
+        {
+
+            var q = Db.Query(entityName).Unique(source);
+
+            if (source.ContainsKey(Db.config.id) && !source[Db.config.id]!.IsNullOrEmptyOrDbNull())
+                q.Where("($" + Db.config.id + " != @0)").Parameters(source[Db.config.id]!);
+
+            return q.DictCache();
+        }
+
+        public void Persist(EntityValues v)
+        {
+            if (v.Get(Db.config.id).IsNullOrEmptyOrDbNull())
+            {
+                v.Default().Reset();
+                var p = Db.Persist(v.entityName).Insert(v.values).Exec().RemoveCache();
+            }
+            else
+            {
+                v.Reset();
+                var p = Db.Persist(v.entityName).Update(v.values).Exec().RemoveCache();
+            }
+        }
+
+
+        public IDictionary<string, object>? RowByUniqueFieldOrValues(string fieldName, EntityValues values)
+        {
+            try { 
+                if (Db.Field(values.entityName, fieldName).IsUnique())
+                    return RowByFieldValue(values.entityName, fieldName, values.Get(fieldName));
+                else
+                    return RowByUniqueWithoutIdIfExists(values.entityName, values.Get());
+            } catch (Exception ex)
+            {
+                return null;
+            }
         }
 
     }
