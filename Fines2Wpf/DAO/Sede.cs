f8d440d1811ad13cf6662bc6c323e1280be76013
Iván Castañeda <icastaneda@scba.gov.ar>
2023-10-18 08:54:48 -0300

    Redefinir metodos.

--- Fines2Wpf/DAO/Sede.cs
+++ Fines2Wpf/DAO/Sede.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿using SqlOrganize;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Utils;
 
 namespace Fines2Wpf.DAO
 {
     public class Sede
     {
-        public IEnumerable<Dictionary<string, object>> BusquedaAproximada(string search)
+        public EntityQuery BusquedaAproximadaQuery(string search)
         {
             return ContainerApp.db.Query("sede").
                 Fields("id, nombre").
@@ -18,8 +20,16 @@ namespace Fines2Wpf.DAO
                     OR $numero LIKE @1
                 ")
                 .Parameters("%" + search + "%", "%" + search + "%")
-                .Order("$nombre ASC").ColOfDictCache();
+                .Order("$nombre ASC");
         }
+
+        public IEnumerable<object> IdsSedesActivasSemestre(object anio, object semestre)
+        {
+            var comisionDAO = new DAO.Comision();
+            var r = comisionDAO.ComisionesAutorizadasPorSemestreQuery(anio, semestre).ColOfDictCache();
+            return r.ColOfVal<object>("sede");
+        }
+
 
     }
 }
