8ea3008915560b581d5d8c3a92a9ed00191d4915
ivancas84 <ivancas84@gmail.com>
2024-03-08 12:44:42 -0300

    Cargar actualizaciones de SqlOrganize.

---
+++ Fines2Wpf/WpfUtils/Windows/ProgressBar.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using Utils;
+
+namespace GapApp.WpfUtils.Windows
+{
+    /// <summary>
+    /// Lógica de interacción para ProgressBar.xaml v1
+    /// </summary>
+    public partial class ProgressBar : Window
+    {
+        public ProgressBar(string? title = null)
+        {
+            if(!title.IsNullOrEmptyOrDbNull())
+                this.Title = title;
+
+            InitializeComponent();
+        }
+
+        public void UpdateProgress(int percentage)
+        {
+            // When progress is reported, update the progress bar control.
+            pbLoad.Value = percentage;
+
+            // When progress reaches 100%, close the progress bar window.
+            if (percentage == 100)
+            {
+                Close();
+            }
+        }
+    }
+}
